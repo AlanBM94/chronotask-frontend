6bfdfe91b88e2e03e2fda6e62d4791355101d8a9
AlanBM94 <sevilbm94@hotmail.com>
2021-04-27 18:19:57 -0500

    Show spinner and messages in authentication

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -2,11 +2,16 @@ import React, { useState } from 'react';
 import Logo from './../../images/logo';
 import Login from '../../components/LogIn';
 import SignUp from '../../components/SignUp';
-import './auth.scss';
 import background from './../../images/background.jpg';
+import { useTypedSelector } from './../../hooks/use-typed-selector';
+import Spinner from './../../components/Spinner';
+import Toast from './../../components/Toast';
+
+import './auth.scss';
 
 const Authentication = () => {
     const [formType, setFormType] = useState('signup');
+    const auth = useTypedSelector((state) => state.auth);
 
     const changeFormHandler = () => {
         if (formType === 'signup') {
@@ -21,9 +26,21 @@ const Authentication = () => {
             <div className="authentication__image">
                 <img src={background} alt="background" />
             </div>
+            {auth.error && (
+                <Toast info={{ message: auth.error, type: 'error' }} />
+            )}
+            {auth.user && (
+                <Toast
+                    info={{
+                        message: `Bienvenido ${auth.user.data.user.name}`,
+                        type: 'success',
+                    }}
+                />
+            )}
 
             <div className="authentication__form">
-                <Logo />
+                {auth.loading ? <Spinner /> : <Logo />}
+
                 {formType === 'signup' && (
                     <SignUp changeFormHandler={changeFormHandler} />
                 )}
